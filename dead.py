e914b9886cfad18ffdad8b0b76daf1c6142e08af
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-05 15:28:09 -0500

    move the web dir

--- dead.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>hello new dead drop☠️</h1>"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
