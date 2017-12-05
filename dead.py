36713821bb4456f3c51890bf960f0ee4778f725f
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-05 12:19:02 -0500

    restart uwsgi

--- dead.py
+++ dead.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>new dead drop☠️</h1>"
+    return "<h1 style='color:blue'>hello new dead drop☠️</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
