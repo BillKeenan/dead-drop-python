920e4f2e1da40e443ab15be7bf6c59435b567a25
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-05 16:15:44 -0500

    fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-scp $arv[1]/wsgi.py bkeenan@dev.dead-drop.me:~/dead
-scp $arv[1]/dead.py bkeenan@dev.dead-drop.me:~/dead
-scp -r $arv[1]/deadweb bkeenan@dev.dead-drop.me:~/dead
+scp $1/wsgi.py bkeenan@dev.dead-drop.me:~/dead
+scp $1/dead.py bkeenan@dev.dead-drop.me:~/dead
+scp -r $1/deadweb bkeenan@dev.dead-drop.me:~/dead
 ssh bkeenan@dev.dead-drop.me 'sudo systemctl restart dead'
