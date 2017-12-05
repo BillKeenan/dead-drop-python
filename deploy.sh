6b4dbcc8689f96cc83d2ea2cbeec8fa01cbf6966
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-05 16:39:09 -0500

    add bash

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 scp $1/wsgi.py bkeenan@dev.dead-drop.me:~/dead
-scp $1/dead.py bkeenan@dev.dead-drop.me:~/dead
-scp -r $1/deadweb bkeenan@dev.dead-drop.me:~/dead
+scp -r $1/deadWeb bkeenan@dev.dead-drop.me:~/dead
 ssh bkeenan@dev.dead-drop.me 'sudo systemctl restart dead'
