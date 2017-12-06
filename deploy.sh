c699304765a9ec21789fb1be60fc9dfa7b89c6d1
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-06 11:59:40 -0500

    copy requirements as well

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 scp $1/wsgi.py bkeenan@dev.dead-drop.me:~/dead
+scp $1/requirements.txt bkeenan@dev.dead-drop.me:~/dead
 scp -r $1/deadWeb bkeenan@dev.dead-drop.me:~/dead
 ssh bkeenan@dev.dead-drop.me 'sudo systemctl restart dead'
