b68bd15546e3007f18f0402f12e4266b66b638b8
Bill Keenan <bill@bigmojo.net>
2017-12-06 23:17:06 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ scp $1/wsgi.py bkeenan@dev.dead-drop.me:~/dead
 scp $1/requirements.txt bkeenan@dev.dead-drop.me:~/dead
 scp -r $1/deadWeb bkeenan@dev.dead-drop.me:~/dead
 ssh bkeenan@dev.dead-drop.me 'sudo systemctl restart dead'
+ssh bkeenan@dev.dead-drop.me 'cd dead;source dead/bin/activate;pip install -r requirements;'
