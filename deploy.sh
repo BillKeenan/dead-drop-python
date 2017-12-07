d33a7d3e11406c2525a144951fb80cb4a8f7bb18
william keenan <williamkeenan@williams-MacBook-Pro-2.local>
2017-12-07 15:47:56 -0500

    should be copying coverage report

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 scp $1/wsgi.py bkeenan@dev.dead-drop.me:~/dead
 scp $1/requirements.txt bkeenan@dev.dead-drop.me:~/dead
 scp -r $1/deadWeb bkeenan@dev.dead-drop.me:~/dead
+scp -r /tmp/coverage bkeenan@dev.dead-drop.me:~/dead
 ssh bkeenan@dev.dead-drop.me 'sudo systemctl restart dead'
 ssh bkeenan@dev.dead-drop.me 'cd dead;source deaddrop/bin/activate;pip install -r requirements.txt'
